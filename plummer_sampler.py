5616a5cfd84b362810b68b74416f770da1147740
Christian Hayes <chrishayesastro@gmail.com>
2021-12-22 14:34:14 -0800

    fix uniform sphere sampling

--- plummer_sampler.py
+++ plummer_sampler.py
@@ -8,7 +8,8 @@ class plummer_model:
     def _plummer_sample(self, rh, size):
         self.rad = self._radius_sample(rh, size)
         self.phi = np.random.random(size=size) * 2. * np.pi
-        self.theta = (np.random.random(size=size) * np.pi) - np.pi / 2.
+        self.theta = np.arccos(
+            1. - 2. * np.random.random(size=size)) - np.pi / 2.
 
     def _radius_sample(self, rh, size):
         a = np.sqrt((1 / (0.5**(2 / 3))) - 1) * rh
